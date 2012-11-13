531adf35e99cba2d14ed25ebea2a3f8fe873c6ee
Eric Mill <konklone@gmail.com>
2012-11-13 14:32:08 -0500

    Initial commit

---
+++ src/com/sunlightfoundation/sitegeist/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sunlightfoundation.sitegeist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
